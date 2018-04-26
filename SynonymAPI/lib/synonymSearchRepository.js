6f3d286ab7bfff7f8ff5db7721e5c90cd4e88ce0
taihoinst1 <gjlim@taihoinst.com>
2018-04-26 15:54:40 +0900

    유의어 등록/수정/조회 추가 및 수정

--- SynonymAPI/lib/synonymSearchRepository.js
+++ SynonymAPI/lib/synonymSearchRepository.js
@@ -7,6 +7,7 @@ var sql = require('mssql');
 module.exports = {
     // 스웨거 스팩상 라우터에서 패스 파라미터는 단일항목만 지원함
 
+    // TODO : 삭제 (사용안함)
     selectWord: function (req, res) {
         //var args = dbHelper.sqlSelectParameters(req);
         var sqlText =
@@ -17,6 +18,7 @@ module.exports = {
         //console.log(res);
         return dbHelper.sqlSelect(sqlText, res);
     },
+    // TODO : 삭제 (사용안함)
     updateWord: function (req, res) {
         var sqlText =
             "   SELECT WORD \n" +
@@ -25,13 +27,48 @@ module.exports = {
 
         dbHelper.sqlSelect(sqlText, res)
 
-        
+
         console.log(res);
 
         var sqlText =
             "       UPDATE TBL_SYNONYM SET "
             + "     SYNONYM = " + req.query.synonym + "\n"
             + "     WHERE WORD LIKE '%" + req.query.word + "%' \n";
+
+        return dbHelper.sqlExecute(sqlText, res);
+    },
+    selectWordSynonList: function (req, res) {
+        var sqlText =
+            "       SELECT SS.WORD_SEQ as wordSeq,     \n"
+            + "            SS.SYNON_SEQ as synonSeq,   \n"
+            + "            SS.WORD as word,            \n"
+            + "            SS.WORD_SYNON as wordSynon, \n"
+            + "            SS.WORD_SYNON_SCORE as wordSynonScore, \n"
+            + "            SS.SYNONYM as synonym       \n"
+            + "     FROM TBL_WORD_SYNON_SCORE SS       \n"
+            + "     WHERE SS.WORD = '" + req.query.word + "' \n";
+        + "     ORDER BY SS.WORD_SEQ, SS.WORD_SYNON_SCORE DESC, SS.SYNON_SEQ \n"
+        return dbHelper.sqlSelect(sqlText, res);
+    },
+    updateWordSynonScore: function (req, res) {
+        var sqlText = [];
+        sqlText.push(
+            "       UPDATE TBL_WORD_SYNON_SCORE \n"
+            + "     SET WORD_SYNON_SCORE = (WORD_SYNON_SCORE + 1) \n"
+            + "     WHERE WORD_SEQ = " + req.body[0].wordSeq + " \n"
+            + "      AND SYNON_SEQ = " + req.body[0].synonSeq + " \n"
+        );
+
+        return dbHelper.sqlExecute(sqlText, res)
+    },
+    insertWordSynonScore: function (req, res) {
+        var sqlText = [];
+        sqlText.push(
+            "       INSERT INTO TBL_WORD_SYNON_SCORE (WORD_SEQ, WORD, WORD_SYNON, WORD_SYNON_SCORE, SYNONYM) \n"
+            + "     SELECT WORD_SEQ, WORD, '" + req.body[0].wordSynon + "', 0, SYNONYM FROM TBL_WORD_SYNON  \n"
+            + "      WHERE WORD_SEQ = " + req.body[0].wordSeq + "\n"
+            /** + "        AND WORD     = '" + req.body[0].word + "' \n" */
+        );
 
         return dbHelper.sqlExecute(sqlText, res);
     }
